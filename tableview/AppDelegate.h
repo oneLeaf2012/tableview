59934b1926c1fb7a11751b8b665ee8c973fc9247
shuzhenguo <shuzhen.guo@bj.china.com>
2017-06-11 20:54:40 +0800

    Initial Commit

---
+++ tableview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tableview
+//
+//  Created by shuzhenguo on 2017/6/11.
+//  Copyright © 2017年 shuzhenguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tableview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tableview
+//
+//  Created by shuzhenguo on 2017/6/11.
+//  Copyright © 2017年 shuzhenguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
